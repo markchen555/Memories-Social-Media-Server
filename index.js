a70854878034bbf6eca607c9e847bba352f34066
Mark Chen <markchen555@gmail.com>
2023-07-10 22:39:02 -0700

    [FE] add a comment for CORS

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-app.use(cors());
+app.use(cors()); // cors has to be above route
 
 app.use('/posts', postRoutes); // posts routes
 
